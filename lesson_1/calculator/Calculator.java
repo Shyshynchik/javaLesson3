eb010c9f32dac9befcdac354c676f0f9f9553ae2
igor <igor.kostenko.1999@mail.ru>
2022-12-05 21:53:25 +0300

    l1 refactor

--- lesson_1/calculator/Calculator.java
+++ lesson_1/calculator/Calculator.java
@@ -1,7 +1,6 @@
 import java.util.ArrayDeque;
 import java.util.Deque;
 import java.util.regex.MatchResult;
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Calculator {
@@ -10,57 +9,55 @@ public class Calculator {
     private Deque<String> symbols;
     private String lastOperation;
 
+    private static final Pattern pattern = Pattern.compile("-?\\d+|[+\\-*/]|[(\\)/]");
+
     public int calculate(String expression) {
 
-        String[] expressionsElements = this.getExpressionElements(expression);
+        String[] expressionsElements = getExpressionElements(expression);
 
-        this.numbers = new ArrayDeque<>();
-        this.symbols = new ArrayDeque<>();
+        numbers = new ArrayDeque<>();
+        symbols = new ArrayDeque<>();
 
         for (String element : expressionsElements) {
-            this.parseElement(element);
+            resolveElement(element);
         }
 
-        while (!this.symbols.isEmpty()) {
-            this.calculateValue();
+        while (!symbols.isEmpty()) {
+            calculateValue();
         }
 
         return Integer.parseInt(numbers.pop());
     }
 
     private String[] getExpressionElements(String expression) {
-        Pattern pattern = Pattern.compile("-?\\d+|[+\\-*/]|[(\\)/]");
-
-        Matcher matcher = pattern.matcher(expression);
-
-        return matcher.results().map(MatchResult::group).toArray(String[]::new);
+        return pattern.matcher(expression).results().map(MatchResult::group).toArray(String[]::new);
     }
 
-    private void parseElement(String element) {
-        if (this.isNumeric(element)) {
-            this.numbers.push(element);
+    private void resolveElement(String element) {
+        if (isNumeric(element)) {
+            numbers.push(element);
             return;
         }
-        if (this.symbols.isEmpty() || element.equals("(")) {
-            this.symbols.push(element);
-            this.lastOperation = element;
+        if (symbols.isEmpty() || element.equals("(")) {
+            symbols.push(element);
+            lastOperation = element;
             return;
         }
         if (element.equals(")")) {
-            this.calculateBracket(element);
+            calculateBracket(element);
             return;
         }
 
-        int operationPriority = this.getOperationPriority(element);
-        int lastOperationPriority = this.getOperationPriority(lastOperation);
+        int operationPriority = getOperationPriority(element);
+        int lastOperationPriority = getOperationPriority(lastOperation);
         if (operationPriority <= lastOperationPriority) {
-            this.calculateValue();
-            this.symbols.push(element);
+            calculateValue();
+            symbols.push(element);
             return;
         }
 
-        this.symbols.push(element);
-        this.lastOperation = element;
+        symbols.push(element);
+        lastOperation = element;
     }
 
     private boolean isNumeric(String str) {
@@ -81,32 +78,32 @@ public class Calculator {
     private void calculateBracket(String symbol) {
         while (!symbol.equals("(")) {
             calculateValue();
-            symbol = this.symbols.pop();
+            symbol = symbols.pop();
         }
 
-        if (!this.symbols.isEmpty()) {
-            this.lastOperation = this.symbols.pop();
-            this.symbols.push(lastOperation);
+        if (!symbols.isEmpty()) {
+            lastOperation = symbols.pop();
+            symbols.push(lastOperation);
         }
     }
 
     private void calculateValue() {
-        String operation = this.symbols.pop();
-        Integer second = Integer.parseInt(this.numbers.pop());
-        Integer first = Integer.parseInt(this.numbers.pop());
+        String operation = symbols.pop();
+        Integer second = Integer.parseInt(numbers.pop());
+        Integer first = Integer.parseInt(numbers.pop());
 
 
         if (operation.equals("+")) {
-            this.numbers.push(String.valueOf(first + second));
+            numbers.push(String.valueOf(first + second));
         }
         if (operation.equals("-")) {
-            this.numbers.push(String.valueOf(first - second));
+            numbers.push(String.valueOf(first - second));
         }
         if (operation.equals("*")) {
-            this.numbers.push(String.valueOf(first * second));
+            numbers.push(String.valueOf(first * second));
         }
         if (operation.equals("/")) {
-            this.numbers.push(String.valueOf(first / second));
+            numbers.push(String.valueOf(first / second));
         }
     }
 }
--- lesson_1/class_loader/ClassLoaderTest.java
+++ lesson_1/class_loader/ClassLoaderTest.java
@@ -1,10 +1,12 @@
+import java.util.HashMap;
+
 public class ClassLoaderTest {
     public static void main(String[] args) {
         ClassLoaderTest that = new ClassLoaderTest();
 
         System.out.println("ClassLoaderTest class classloader - " + ClassLoaderTest.class.getClassLoader());
 
-        var hm = new java.util.HashMap();
+        var hm = new HashMap();
     }
 
     public void helloWorld() {
